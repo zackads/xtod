88e24506429d4dc706af06a63837e26ae253f4a6
Zack Adlington <zackads@users.noreply.github.com>
2026-01-27 12:23:51 +0000

    Initial Commit

---
+++ xtod/xtodApp.swift
@@ -0,0 +1,17 @@
+//
+//  xtodApp.swift
+//  xtod
+//
+//  Created by Zack Adlington on 27/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct xtodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
